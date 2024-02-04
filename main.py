d96a42226b3ed38da3c926bb4162ee7eb49a0d19
Rodmatronics <roderickmpike@icloud.com>
2024-02-04 14:58:55 -0300

    Added nice border and colour

--- main.py
+++ main.py
@@ -11,5 +11,13 @@ root.overrideredirect(True)
 width, height = 2080, 50
 root.attributes('-topmost', True)
 root.geometry(f"{width}x{height}+0+1032")
+root.configure(background='#1B3769')
+
+border_canvas = tk.Canvas(root, width=width, height=height, background='#1B3769', highlightthickness=0)
+border_canvas.pack()
+
+border_width = 3
+border_canvas.create_rectangle(0, 0, width, border_width, fill='#0D204D', outline='#0D204D')  # Top border
+border_canvas.create_rectangle(0, 0, border_width, height, fill='#0D204D', outline='#0D204D')  # Left border
 
 root.mainloop()
